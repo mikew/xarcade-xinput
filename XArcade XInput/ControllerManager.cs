5d7b73551d761f1f617cd6ac3dc7ddde8086315f
Mike Wyatt <wyatt.mike@gmail.com>
2017-04-16 06:54:44 -0300

    Initial

---
+++ XArcade XInput/ControllerManager.cs
@@ -0,0 +1,118 @@
+﻿using ScpDriverInterface;
+
+namespace XArcade_XInput {
+    class ControllerManagerEventArgs : System.EventArgs {
+        public int Index;
+        public X360Controller Controller;
+    }
+
+    public enum X360Axis {
+        LeftTrigger,
+        RightTrigger,
+        LeftStickX,
+        LeftStickY,
+        RightStickX,
+        RightStickY,
+    }
+
+    class ControllerManager {
+        public event System.EventHandler<ControllerManagerEventArgs> OnChange;
+
+        X360Controller[] controllers = new X360Controller[] {
+            new X360Controller(),
+            new X360Controller(),
+            new X360Controller(),
+            new X360Controller(),
+        };
+
+        public void ButtonDown (int Index, X360Buttons Button) {
+            var current = controllers[Index];
+            var next = new X360Controller(current);
+            next.Buttons |= Button;
+
+            if (current.Buttons == next.Buttons) {
+                return;
+            }
+
+            controllers[Index] = next;
+            InvokeChange(Index);
+        }
+
+        public void ButtonUp (int Index, X360Buttons Button) {
+            var current = controllers[Index];
+            var next = new X360Controller(current);
+            next.Buttons &= ~Button;
+
+            if (current.Buttons == next.Buttons) {
+                return;
+            }
+
+            controllers[Index] = next;
+            InvokeChange(Index);
+        }
+
+        public void SetAxis (int Index, X360Axis Axis, int Value) {
+            int current = int.MinValue;
+            var controller = controllers[Index];
+
+            switch (Axis) {
+                case X360Axis.LeftTrigger:
+                    current = controller.LeftTrigger;
+                    break;
+                case X360Axis.RightTrigger:
+                    current = controller.RightTrigger;
+                    break;
+                case X360Axis.LeftStickX:
+                    current = controller.LeftStickX;
+                    break;
+                case X360Axis.LeftStickY:
+                    current = controller.LeftStickY;
+                    break;
+                case X360Axis.RightStickX:
+                    current = controller.RightStickX;
+                    break;
+                case X360Axis.RightStickY:
+                    current = controller.RightStickY;
+                    break;
+            }
+
+            if (current == int.MinValue) {
+                return;
+            }
+
+            if (current == Value) {
+                return;
+            }
+
+            switch (Axis) {
+                case X360Axis.LeftTrigger:
+                    controller.LeftTrigger = (byte)Value;
+                    break;
+                case X360Axis.RightTrigger:
+                    controller.RightTrigger = (byte)Value;
+                    break;
+                case X360Axis.LeftStickX:
+                    controller.LeftStickX = (short)Value;
+                    break;
+                case X360Axis.LeftStickY:
+                    controller.LeftStickY = (short)Value;
+                    break;
+                case X360Axis.RightStickX:
+                    controller.RightStickX = (short)Value;
+                    break;
+                case X360Axis.RightStickY:
+                    controller.RightStickY = (short)Value;
+                    break;
+            }
+
+            InvokeChange(Index);
+        }
+
+        void InvokeChange (int Index) {
+            OnChange?.Invoke(null, new ControllerManagerEventArgs {
+                Index = Index,
+                Controller = controllers[Index],
+            });
+        }
+    }
+}
--- XArcade XInput/Program.cs
+++ XArcade XInput/Program.cs
@@ -1,12 +1,192 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ScpDriverInterface;
 
 namespace XArcade_XInput {
     class Program {
+        static Gma.System.MouseKeyHook.IKeyboardMouseEvents KeyboardHook;
+        static ControllerManager Manager;
+        static ScpBus Bus;
+        static bool IsRunning = false;
+
         static void Main (string[] args) {
+            KeyboardHook = Gma.System.MouseKeyHook.Hook.GlobalEvents();
+            Manager = new ControllerManager();
+            Bus = new ScpBus();
+
+            Start();
+
+            // See https://github.com/gmamaladze/globalmousekeyhook/issues/3#issuecomment-230909645
+            System.Windows.Forms.ApplicationContext msgLoop = new System.Windows.Forms.ApplicationContext();
+            System.Windows.Forms.Application.Run(msgLoop);
+        }
+
+        static void Start () {
+            Bus.PlugIn(1);
+            Bus.PlugIn(2);
+            Manager.OnChange += Manager_OnChange;
+            KeyboardHook.KeyDown += KeyboardHook_KeyDown;
+            KeyboardHook.KeyUp += KeyboardHook_KeyUp;
+            IsRunning = true;
+            System.Console.WriteLine("Started.");
+        }
+
+        static void Stop () {
+            Bus.UnplugAll();
+            Manager.OnChange -= Manager_OnChange;
+            KeyboardHook.KeyDown -= KeyboardHook_KeyDown;
+            KeyboardHook.KeyUp -= KeyboardHook_KeyUp;
+            IsRunning = false;
+            System.Console.WriteLine("Stopped.");
+        }
+
+        private static void Manager_OnChange (object sender, ControllerManagerEventArgs e) {
+            Bus.Report(e.Index + 1, e.Controller.GetReport());
+        }
+
+        private static void KeyboardHook_KeyDown (object sender, System.Windows.Forms.KeyEventArgs e) {
+            var isHandled = false;
+
+            switch (e.KeyCode) {
+                case System.Windows.Forms.Keys.Escape:
+                    isHandled = true;
+                    if (IsRunning) {
+                        Stop();
+                    }
+                    break;
+
+                case System.Windows.Forms.Keys.LShiftKey:
+                    Manager.ButtonDown(0, X360Buttons.A);
+                    isHandled = true;
+                    break;
+                case System.Windows.Forms.Keys.Z:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.B);
+                    break;
+                case System.Windows.Forms.Keys.LControlKey:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.X);
+                    break;
+                case System.Windows.Forms.Keys.Alt:
+                case System.Windows.Forms.Keys.LMenu:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.Y);
+                    break;
+                case System.Windows.Forms.Keys.D1:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.Start);
+                    break;
+                case System.Windows.Forms.Keys.D3:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.Back);
+                    break;
+                case System.Windows.Forms.Keys.C:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.LeftBumper);
+                    break;
+                case System.Windows.Forms.Keys.Space:
+                    isHandled = true;
+                    Manager.ButtonDown(0, X360Buttons.RightBumper);
+                    break;
+
+                case System.Windows.Forms.Keys.Up:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickY, short.MaxValue);
+                    break;
+                case System.Windows.Forms.Keys.Down:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickY, short.MinValue);
+                    break;
+                case System.Windows.Forms.Keys.Left:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickX, short.MinValue);
+                    break;
+                case System.Windows.Forms.Keys.Right:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickX, short.MaxValue);
+                    break;
+
+                case System.Windows.Forms.Keys.D5:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftTrigger, byte.MaxValue);
+                    break;
+                case System.Windows.Forms.Keys.X:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.RightTrigger, byte.MaxValue);
+                    break;
+            }
+
+            if (isHandled) {
+                e.Handled = true;
+            }
+        }
+
+        private static void KeyboardHook_KeyUp (object sender, System.Windows.Forms.KeyEventArgs e) {
+            var isHandled = false;
+
+            switch (e.KeyCode) {
+                case System.Windows.Forms.Keys.LShiftKey:
+                    Manager.ButtonUp(0, X360Buttons.A);
+                    isHandled = true;
+                    break;
+                case System.Windows.Forms.Keys.Z:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.B);
+                    break;
+                case System.Windows.Forms.Keys.LControlKey:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.X);
+                    break;
+                case System.Windows.Forms.Keys.Alt:
+                case System.Windows.Forms.Keys.LMenu:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.Y);
+                    break;
+                case System.Windows.Forms.Keys.D1:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.Start);
+                    break;
+                case System.Windows.Forms.Keys.D3:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.Back);
+                    break;
+                case System.Windows.Forms.Keys.C:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.LeftBumper);
+                    break;
+                case System.Windows.Forms.Keys.Space:
+                    isHandled = true;
+                    Manager.ButtonUp(0, X360Buttons.RightBumper);
+                    break;
+
+                case System.Windows.Forms.Keys.Up:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickY, 0);
+                    break;
+                case System.Windows.Forms.Keys.Down:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickY, 0);
+                    break;
+                case System.Windows.Forms.Keys.Left:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickX, 0);
+                    break;
+                case System.Windows.Forms.Keys.Right:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftStickX, 0);
+                    break;
+
+                case System.Windows.Forms.Keys.D5:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.LeftTrigger, byte.MinValue);
+                    break;
+                case System.Windows.Forms.Keys.X:
+                    isHandled = true;
+                    Manager.SetAxis(0, X360Axis.RightTrigger, byte.MinValue);
+                    break;
+            }
+
+            if (isHandled) {
+                e.Handled = true;
+            }
         }
     }
 }
