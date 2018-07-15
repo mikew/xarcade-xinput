8762ae4817bb31037ac2d5bf61aab62faf6d2de9
Mike Wyatt <wyatt.mike@gmail.com>
2018-07-15 14:45:28 -0300

    fix API basepath

--- webapp/src/api/index.ts
+++ webapp/src/api/index.ts
@@ -1,7 +1,12 @@
 import * as api from './generated/api'
 
 const queryParams = new URLSearchParams(window.location.search)
-const basePath = queryParams.get('API_URL') || undefined
+const urlFromQuery = queryParams.get('API_URL')
+const basePath = urlFromQuery
+  ? urlFromQuery
+  : process.env.NODE_ENV === 'production'
+    ? '/api'
+    : 'http://localhost:32123/api'
 
 export default {
   controller: api.ControllerApiFactory(undefined, undefined, basePath),
